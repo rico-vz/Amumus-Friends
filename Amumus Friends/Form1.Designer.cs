95c056eb00d4caa8062dcd3bfd271438b74e2d96
Rico van Zelst <ricovzelst@pm.me>
2023-03-03 20:48:33 +0100

    [+] Export, populate listbox, icon

--- Amumus Friends/Form1.Designer.cs
+++ Amumus Friends/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Amumus_Friends
 {
-    partial class Form1
+    partial class amumus_friends
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(amumus_friends));
+            exportBtn = new MaterialSkin.Controls.MaterialButton();
+            importBtn = new MaterialSkin.Controls.MaterialButton();
+            deleteAllBtn = new MaterialSkin.Controls.MaterialButton();
+            statusLabel = new MaterialSkin.Controls.MaterialLabel();
+            creditLabel = new MaterialSkin.Controls.MaterialLabel();
+            exportsListBox = new ListBox();
+            SuspendLayout();
+            // 
+            // exportBtn
+            // 
+            exportBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            exportBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            exportBtn.Depth = 0;
+            exportBtn.HighEmphasis = true;
+            exportBtn.Icon = null;
+            exportBtn.Location = new Point(168, 78);
+            exportBtn.Margin = new Padding(4, 6, 4, 6);
+            exportBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            exportBtn.Name = "exportBtn";
+            exportBtn.NoAccentTextColor = Color.Empty;
+            exportBtn.Size = new Size(169, 36);
+            exportBtn.TabIndex = 0;
+            exportBtn.Text = "Export friendslist";
+            exportBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            exportBtn.UseAccentColor = false;
+            exportBtn.UseVisualStyleBackColor = true;
+            exportBtn.Click += exportBtn_Click;
+            // 
+            // importBtn
+            // 
+            importBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            importBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            importBtn.Depth = 0;
+            importBtn.HighEmphasis = true;
+            importBtn.Icon = null;
+            importBtn.Location = new Point(7, 78);
+            importBtn.Margin = new Padding(4, 6, 4, 6);
+            importBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            importBtn.Name = "importBtn";
+            importBtn.NoAccentTextColor = Color.Empty;
+            importBtn.Size = new Size(153, 36);
+            importBtn.TabIndex = 1;
+            importBtn.Text = "Import from file";
+            importBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            importBtn.UseAccentColor = false;
+            importBtn.UseVisualStyleBackColor = true;
+            importBtn.Click += importBtn_Click;
+            // 
+            // deleteAllBtn
+            // 
+            deleteAllBtn.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            deleteAllBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            deleteAllBtn.Depth = 0;
+            deleteAllBtn.HighEmphasis = true;
+            deleteAllBtn.Icon = null;
+            deleteAllBtn.Location = new Point(345, 78);
+            deleteAllBtn.Margin = new Padding(4, 6, 4, 6);
+            deleteAllBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            deleteAllBtn.Name = "deleteAllBtn";
+            deleteAllBtn.NoAccentTextColor = Color.Empty;
+            deleteAllBtn.Size = new Size(166, 36);
+            deleteAllBtn.TabIndex = 2;
+            deleteAllBtn.Text = "Delete all Friends";
+            deleteAllBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            deleteAllBtn.UseAccentColor = false;
+            deleteAllBtn.UseVisualStyleBackColor = true;
+            deleteAllBtn.Click += deleteAllBtn_Click;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.Depth = 0;
+            statusLabel.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            statusLabel.Location = new Point(6, 237);
+            statusLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(154, 19);
+            statusLabel.TabIndex = 3;
+            statusLabel.Text = "LCU Status: Unknown";
+            // 
+            // creditLabel
+            // 
+            creditLabel.AutoSize = true;
+            creditLabel.Depth = 0;
+            creditLabel.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            creditLabel.Location = new Point(413, 237);
+            creditLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            creditLabel.Name = "creditLabel";
+            creditLabel.Size = new Size(97, 19);
+            creditLabel.TabIndex = 5;
+            creditLabel.Text = "Made by Rico";
+            // 
+            // exportsListBox
+            // 
+            exportsListBox.FormattingEnabled = true;
+            exportsListBox.ItemHeight = 15;
+            exportsListBox.Location = new Point(7, 123);
+            exportsListBox.Name = "exportsListBox";
+            exportsListBox.Size = new Size(153, 94);
+            exportsListBox.TabIndex = 6;
+            exportsListBox.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // amumus_friends
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(518, 262);
+            Controls.Add(exportsListBox);
+            Controls.Add(creditLabel);
+            Controls.Add(statusLabel);
+            Controls.Add(deleteAllBtn);
+            Controls.Add(importBtn);
+            Controls.Add(exportBtn);
+            FormStyle = FormStyles.ActionBar_48;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "amumus_friends";
+            Padding = new Padding(3, 72, 3, 3);
+            Text = "Amumus Friends";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialButton exportBtn;
+        private MaterialSkin.Controls.MaterialButton importBtn;
+        private MaterialSkin.Controls.MaterialButton deleteAllBtn;
+        private MaterialSkin.Controls.MaterialLabel statusLabel;
+        private MaterialSkin.Controls.MaterialLabel creditLabel;
+        private ListBox exportsListBox;
     }
 }
--- Amumus Friends/Form1.cs
+++ Amumus Friends/Form1.cs
@@ -1,10 +1,118 @@
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using PoniLCU;
+using static PoniLCU.LeagueClient;
+using Newtonsoft.Json;
+using System.Globalization;
+
 namespace Amumus_Friends
 {
-    public partial class Form1 : Form
+    public partial class amumus_friends : MaterialForm
     {
-        public Form1()
+        public static LeagueClient leagueClient = new LeagueClient(credentials.cmd);
+
+        public amumus_friends()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green400, Primary.Green500, Primary.Green600, Accent.Green100, TextShade.WHITE);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string exportsFolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Exports");
+            if (Directory.Exists(exportsFolder))
+            {
+                string[] files = Directory.GetFiles(exportsFolder);
+                if (files.Length > 0)
+                {
+                    // Populate the list box with the last update dates of the files
+                    foreach (string file in files)
+                    {
+                        string formattedDate = File.GetLastWriteTime(file).ToString("yyyy-MM-dd HH:mm:ss");
+                        exportsListBox.Items.Add(formattedDate);
+                    }
+                }
+                else
+                {
+                    // If there are no files, disable the list box
+                    exportsListBox.Enabled = false;
+                }
+            }
+            else
+            {
+                // If the folder doesn't exist, disable the list box
+                exportsListBox.Enabled = false;
+            }
+        }
+
+
+        private void deleteAllBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void exportBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var friendListStr = await leagueClient.Request(requestMethod.GET, "lol-game-client-chat/v1/buddies");
+
+                List<string> friendList = JsonConvert.DeserializeObject<List<string>>(friendListStr);
+                List<object> friendDataList = new List<object>();
+                foreach (string friend in friendList)
+                {
+                    var ok = friend;
+                    var friendDataStr = await leagueClient.Request(requestMethod.GET, "/lol-summoner/v1/summoners?name=" + friend);
+                    JObject friendDataObj = JsonConvert.DeserializeObject<JObject>(friendDataStr);
+                    var friendInternalName = friendDataObj["internalName"].ToString();
+                    var friendPuuid = friendDataObj["puuid"].ToString();
+
+                    // Add the friend data to the list
+                    friendDataList.Add(new { InternalName = friendInternalName, puuid = friendPuuid });
+
+                    // Don't wanna spam the API too much so we'll wait a bit (This delay is sufficient according to my tests)
+                    await Task.Delay(100);
+                }
+
+                // Create the "Exports" folder if it doesn't exist
+                string exportsFolder = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Exports");
+                if (!Directory.Exists(exportsFolder))
+                {
+                    Directory.CreateDirectory(exportsFolder);
+                }
+
+                string fileName = "exported_friends_" + DateTime.Now.ToString("yyyyMMddHHmmsstt") + ".json";
+                string filePath = Path.Combine(exportsFolder, fileName);
+                using (StreamWriter file = new StreamWriter(filePath))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, friendDataList);
+                }
+                MessageBox.Show("Exported friends to " + filePath, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        private void importBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
